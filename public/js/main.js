6fc4d965ba4ee350ba6d3010fe66fd5bdbcccbf2
rojascastilloignacio8-ops <rojascastilloignacio8@gmail.com>
2026-07-24 08:52:10 -0400

    Se obtiene elemento input para js

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,2 @@
-let x = 0
-if (x = 0){
-    console.log("Es 0")
-    alert("es 0")
-} else{
-    alert("NO es 0")
-    console.log("NO es 0")
-}
+const nombre = document.getElementById("nombre")
+console.log(nombre)

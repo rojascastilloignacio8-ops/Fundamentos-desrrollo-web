3f5015e5880d5c6e6702a71a785d76c6cdf4564d
rojascastilloignacio8-ops <rojascastilloignacio8@gmail.com>
2026-07-27 11:15:46 -0400

    Se agrega la estructura de consulta

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,18 @@ form.addEventListener("submit", (event) =>{
     event.preventDefault()
     console.log(nombre.value)
 })
+
+async function getData() {
+  const url = "http://127.0.0.1:8000/productos";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}

c490f5d9975aea5f3eb3a0f3776432e124017cfc
rojascastilloignacio8-ops <rojascastilloignacio8@gmail.com>
2026-07-28 14:52:33 -0400

    Se completa la estructura cors

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,41 @@
-const nombre = document.getElementById("nombre")
-const form = document.getElementById("frm")
-form.addEventListener("submit", (event) =>{
-    event.preventDefault()
-    console.log(nombre.value)
-})
+const formulario = document.getElementById("frm")
+const input = document.getElementById("categoria-id")
 
-async function getData() {
-  const url = "http://127.0.0.1:8000/productos";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+
+async function obtenerCategorias(){
+  const url = "http://localhost:8000/categoria"
+  try{
+    const respuesta = await fetch(url)
+    if (!respuesta.ok){
+      throw new Error(`Response status: ${respuesta.status}`);
     }
-
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
+    const resultado = await respuesta.json()
+    console.log(resultado)
+  }catch(Error){
     console.error(error.message);
   }
 }
+async function obtenerCategoriaID(categoria_id){
+  const url = `http://localhost:8000/categorias/${categoria_id}`
+  try{
+    const respuesta = await fetch(url)
+    if (!respuesta.ok){
+      throw new Error(`Response status: ${respuesta.status}`);
+    }
+    const resultado = await respuesta.json()
+    console.log(resultado)
+  }catch(Error){
+    console.error(error.message);
+  }
+}
+formulario.addEventListener("submit", (event)=>{
+  event.preventDefault()
+  let ob = [obtenerCategoriaID(input.value)]
+  let tex = document.getElementById("tex")
+  let caj = document.getElementById("caj")
+  caj.appendChild(tex)
+  for (producto in ob){
+    console.log()
+  }
+})
+obtenerCategorias()
